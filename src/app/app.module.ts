5e5870a35288c8f612d5ef387cf6f29d5f8fbb46
kellerman-mota <kellermanpaulo@hotmail.com>
2018-01-19 18:04:24 -0200

    evoluiu projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
+import { AppRoutingModule } from './app-routing.module';
+import { VeiculosComponent } from './veiculos/veiculos.component';
+import { VeiculoService } from './veiculos/veiculo.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    ClientesComponent, VeiculosComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,    
   ],
-  providers: [],
+  providers: [ClienteService, VeiculoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
